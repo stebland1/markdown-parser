4c22ea6085f42c35ff0d2e1925d59dcee8f232dc
Stephen <stebland1@gmail.com>
2025-06-28 18:56:18 +0100

    Parse front matter into key val pairs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
+
+#define MAX_LINES 256
+#define MAX_ENTRIES 50
+
+typedef struct {
+  char key[64];
+  char value[256];
+} Entry;
+
+int parse_front_matter(Entry *cur, char *line) {
+  char *delimiter = strchr(line, ':');
+  if (delimiter == NULL) {
+    return -1;
+  }
+
+  *delimiter = '\0';
+  trim(line);
+  strcpy(cur->key, line);
+  char *value = delimiter + 1;
+  value[strcspn(value, "\n")] = '\0';
+  trim(value);
+  strcpy(cur->value, value);
+
+  return 0;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -16,9 +43,28 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  int c;
-  while ((c = fgetc(file)) != EOF) {
-    printf("%c", c);
+  int in_front_matter = 0;
+  char line[MAX_LINES];
+
+  while (fgets(line, sizeof(line), file)) {
+    if (strncmp("---", line, 3) == 0) {
+      if (in_front_matter) {
+        printf("Finished processing front matter.\n");
+        break;
+      } else {
+        printf("Processing front matter...\n");
+        in_front_matter = 1;
+      }
+    }
+
+    Entry entries[MAX_ENTRIES];
+    int entry_count = 0;
+
+    if (in_front_matter &&
+        parse_front_matter(&entries[entry_count], line) == 0) {
+      printf("%s: %s\n", entries[entry_count].key, entries[entry_count].value);
+      entry_count++;
+    }
   }
 
   fclose(file);
