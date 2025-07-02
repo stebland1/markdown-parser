4ac7530fb0c23e0077990122715b5d8f911c28f6
Stephen Bland <stebland1@gmail.com>
2025-07-02 10:59:28 +0100

    handle list items

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include <ctype.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,15 +7,24 @@
 #include <sys/types.h>
 
 #define MAX_LINES 256
-#define INITIAL_FM_CAPACITY 8
+#define LIST_ITEM_CAPACITY_BUF 8
+#define LIST_VALUE_CAPACITY_BUF 4
 
 #define PARSE_ERROR -1
 #define PARSE_SKIP 1
 #define PARSE_OK 0
 
 typedef struct {
-  char key[64];
-  char value[256];
+  char *key;
+  enum { STRING_VAL, LIST_VAL } type;
+  union {
+    char *string_value;
+    struct {
+      char **items;
+      size_t count;
+      size_t capacity;
+    } list_value;
+  };
 } FrontMatterEntry;
 
 typedef struct {
@@ -29,7 +39,7 @@ FrontMatterList *create_front_matter_list() {
     return NULL;
   }
 
-  list->capacity = INITIAL_FM_CAPACITY;
+  list->capacity = LIST_ITEM_CAPACITY_BUF;
   list->count = 0;
   list->entries = malloc(sizeof(FrontMatterEntry) * list->capacity);
   if (!list->entries) {
@@ -41,15 +51,29 @@ FrontMatterList *create_front_matter_list() {
 }
 
 void free_front_matter_list(FrontMatterList *list) {
-  if (list) {
-    free(list->entries);
-    free(list);
+  if (!list)
+    return;
+
+  for (size_t i = 0; i < list->count; i++) {
+    free(list->entries[i].key);
+
+    if (list->entries[i].type == STRING_VAL) {
+      free(list->entries[i].string_value);
+    } else {
+      for (size_t j = 0; j < list->entries[i].list_value.count; j++) {
+        free(list->entries[i].list_value.items[j]);
+      }
+      free(list->entries[i].list_value.items);
+    }
   }
+
+  free(list->entries);
+  free(list);
 }
 
 int insert_front_matter_entry(FrontMatterList *list, FrontMatterEntry *entry) {
   if (list->count + 1 > list->capacity) {
-    list->capacity += INITIAL_FM_CAPACITY;
+    list->capacity += LIST_ITEM_CAPACITY_BUF;
     list->entries =
         realloc(list->entries, sizeof(FrontMatterEntry) * list->capacity);
     if (!list->entries) {
@@ -74,29 +98,47 @@ int parse_front_matter_entry(FrontMatterEntry *cur, char *line) {
     return PARSE_ERROR;
   }
 
-  strcpy(cur->key, escaped_key);
+  cur->key = escaped_key;
+
   char *value = delimiter + 1;
   value[strcspn(value, "\n")] = '\0';
   trim(value);
 
+  if (*value == '\0') {
+    cur->type = LIST_VAL;
+    return PARSE_OK;
+  }
+
   char *escaped_val = escape_json_str(value);
   if (!escaped_val) {
-    free(escaped_key);
+    free(cur->key);
     return PARSE_ERROR;
   }
 
-  strcpy(cur->value, escaped_val);
-  free(escaped_key);
-  free(escaped_val);
-
+  cur->string_value = escaped_val;
+  cur->type = STRING_VAL;
   return PARSE_OK;
 }
 
 void print_front_matter(FrontMatterList *list) {
   printf("{");
   for (size_t i = 0; i < list->count; i++) {
-    printf("\"%s\": \"%s\"%s", list->entries[i].key, list->entries[i].value,
-           i < list->count - 1 ? "," : "");
+    FrontMatterEntry entry = list->entries[i];
+    printf("\"%s\": ", entry.key);
+
+    switch (entry.type) {
+    case LIST_VAL:
+      printf("[");
+      for (size_t j = 0; j < entry.list_value.count; j++) {
+        printf("\"%s\"%s", entry.list_value.items[j],
+               j < entry.list_value.count - 1 ? "," : "");
+      }
+      printf("]");
+      break;
+    case STRING_VAL:
+      printf("\"%s\"%s", entry.string_value, i < list->count - 1 ? "," : "");
+      break;
+    }
   }
   printf("}");
 }
@@ -124,7 +166,9 @@ int main(int argc, char **argv) {
   }
 
   int in_front_matter = 0;
+  int is_list = 0;
   char line[MAX_LINES];
+
   while (fgets(line, sizeof(line), file)) {
     if (strncmp("---", line, 3) == 0) {
       if (in_front_matter) {
@@ -137,12 +181,61 @@ int main(int argc, char **argv) {
     if (!in_front_matter)
       continue;
 
+    if (is_list) {
+      if (isspace(*line)) {
+        char *p = line;
+        while (isspace(*p))
+          p++;
+        if (*p == '-') {
+          p++;
+          while (isspace(*p))
+            p++;
+
+          FrontMatterEntry *list_entry = &list->entries[list->count - 1];
+          if (list_entry->list_value.count == list_entry->list_value.capacity) {
+            list_entry->list_value.capacity += LIST_VALUE_CAPACITY_BUF;
+            list_entry->list_value.items = (char **)realloc(
+                list_entry->list_value.items,
+                list_entry->list_value.capacity * sizeof(char *));
+
+            if (!list_entry->list_value.items) {
+              fprintf(stderr, "Failed to allocate for list items\n");
+              goto fail;
+            }
+          }
+
+          char *list_item = p;
+          list_item[strcspn(list_item, "\n")] = '\0';
+          trim(list_item);
+          char *escaped_list_item = escape_json_str(list_item);
+          if (!escaped_list_item) {
+            fprintf(stderr, "Failed to allocate for list item\n");
+            goto fail;
+          }
+          list_entry->list_value.items[list_entry->list_value.count++] =
+              escaped_list_item;
+          continue;
+        } else {
+          is_list = 0;
+        }
+      } else {
+        is_list = 0;
+      }
+    }
+
     FrontMatterEntry entry;
     switch (parse_front_matter_entry(&entry, line)) {
     case PARSE_SKIP:
       continue;
     case PARSE_ERROR:
       goto fail;
+    }
+
+    if (entry.type == LIST_VAL && !is_list) {
+      entry.list_value.items = NULL;
+      entry.list_value.count = 0;
+      entry.list_value.capacity = 0;
+      is_list = 1;
     }
 
     if (insert_front_matter_entry(list, &entry) < 0) {
