a6d0bf473ee7185df104219cba87c8be4dfca7ea
Stephen Bland <stebland1@gmail.com>
2025-07-15 11:56:06 +0100

    test(lists): unordered lists

--- tests/test_block.c
+++ tests/test_block.c
@@ -34,3 +34,47 @@ TEST(paragraph) {
 
   return 0;
 }
+
+TEST(unordered_list) {
+  ParserContext ctx;
+  parse_input("- one\n- two\n\n- three\n", &ctx);
+
+  ASSERT_SIZE_EQUALS(1, NUM_CHILDREN(ctx.ast));
+  Token *list = CHILD(ctx.ast, 0);
+  ASSERT_SIZE_EQUALS(3, NUM_CHILDREN(list));
+  ASSERT_STR_EQ("one", CHILD_CONTENT(list, 0));
+  ASSERT_STR_EQ("two", CHILD_CONTENT(list, 1));
+  ASSERT_STR_EQ("three", CHILD_CONTENT(list, 2));
+
+  free_parser_context(&ctx);
+
+  return 0;
+}
+
+TEST(multi_unordered_list) {
+  ParserContext ctx;
+  parse_input("- one\n+ two\n\n* three\n", &ctx);
+
+  ASSERT_SIZE_EQUALS(3, NUM_CHILDREN(ctx.ast));
+  ASSERT_STR_EQ("one", CHILD_CONTENT(CHILD(ctx.ast, 0), 0));
+  ASSERT_STR_EQ("two", CHILD_CONTENT(CHILD(ctx.ast, 1), 0));
+  ASSERT_STR_EQ("three", CHILD_CONTENT(CHILD(ctx.ast, 2), 0));
+
+  free_parser_context(&ctx);
+
+  return 0;
+}
+
+TEST(list_indentation) {
+  ParserContext ctx;
+  parse_input("- one\n  - two\n\n- three\n", &ctx);
+
+  ASSERT_SIZE_EQUALS(3, NUM_CHILDREN(ctx.ast));
+  ASSERT_STR_EQ("one", CHILD_CONTENT(CHILD(ctx.ast, 0), 0));
+  ASSERT_STR_EQ("two", CHILD_CONTENT(CHILD(ctx.ast, 1), 0));
+  ASSERT_STR_EQ("three", CHILD_CONTENT(CHILD(ctx.ast, 2), 0));
+
+  free_parser_context(&ctx);
+
+  return 0;
+}
